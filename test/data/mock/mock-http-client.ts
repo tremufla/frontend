bacf44b84d7bf5feb8f42fd43961c365f841c5ba
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-21 14:50:41 -0300

    feat: adiciona classe HttpPostClientSpy para simulação de requisições POST

--- test/data/mock/mock-http-client.ts
+++ test/data/mock/mock-http-client.ts
@@ -1,5 +1,6 @@
 import { HttpGetClient, HttpGetParams } from "@/data/protocols/http/http-get-client"
 import { HttpResponse, HttpStatusCode } from "@/data/protocols/http/http-response"
+import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
 
 export class HttpGetClientSpy<T, R> implements HttpGetClient<T, R> {
   url?: string
@@ -9,6 +10,20 @@ export class HttpGetClientSpy<T, R> implements HttpGetClient<T, R> {
   }
 
   async get (params: HttpGetParams<T>): Promise<HttpResponse<R>> {
+    this.url = params.url
+    this.body = params.body
+    return Promise.resolve(this.response)
+  }
+}
+
+export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
+  url?: string
+  body?: T
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url
     this.body = params.body
     return Promise.resolve(this.response)
