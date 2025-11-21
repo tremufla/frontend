0bb8e666e1f63bc9402f99e55ec45383cb55a4da
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-21 14:47:38 -0300

    feat: adiciona testes para método post do AxiosHttpClient

--- test/infraestructure/http/axios-http-client/axios-http-client.spec.ts
+++ test/infraestructure/http/axios-http-client/axios-http-client.spec.ts
@@ -42,4 +42,30 @@ describe('AxiosHttpClient', () => {
 
     return expect(promise).resolves.toEqual({ statusCode: axiosResponse.status, body: axiosResponse.data })
   })
+
+  test('Should call axios.post with correct values', async () => {
+    const { sut, mockedAxios } = makeSut()
+
+    const request = { url: 'any_url', body: { any: 'data' } }
+
+    mockedAxios.post.mockResolvedValueOnce({ data: 'any', status: 200 })
+
+    await sut.post(request)
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  test('Should return the correct status code and body for post', () => {
+    const { sut, mockedAxios } = makeSut()
+    const axiosResponse = {
+      data: 'any_data',
+      status: 201
+    }
+
+    mockedAxios.post.mockResolvedValueOnce(axiosResponse)
+
+    const promise = sut.post({ url: 'any_url', body: { any: 'data' } })
+
+    return expect(promise).resolves.toEqual({ statusCode: axiosResponse.status, body: axiosResponse.data })
+  })
 })
