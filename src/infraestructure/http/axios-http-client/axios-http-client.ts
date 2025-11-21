b6524e4158dc8169507929472b1210f267da17e2
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-21 14:03:59 -0300

    feat: refatora AxiosHttpClient para incluir suporte a HttpPostClient

--- src/infraestructure/http/axios-http-client/axios-http-client.ts
+++ src/infraestructure/http/axios-http-client/axios-http-client.ts
@@ -1,14 +1,26 @@
-import { HttpGetClient, HttpGetParams } from '@/data/protocols/http/http-get-client'
-import { HttpResponse } from '@/data/protocols/http/http-response'
-import axios from 'axios'
+import { HttpGetClient, HttpGetParams } from '@/data/protocols/http/http-get-client';
+import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client';
+import { HttpResponse } from '@/data/protocols/http/http-response';
+import axios from 'axios';
 
-export class AxiosHttpClient<Req = unknown, Res = unknown> implements HttpGetClient<Req, Res> {
-  async get (params: HttpGetParams<Req>): Promise<HttpResponse<Res>> {
-    const httpResponse = await axios.get(params.url)
+export class AxiosHttpClient<Req = unknown, Res = unknown>
+  implements HttpGetClient<Req, Res>, HttpPostClient<Req, Res>
+{
+  async get(params: HttpGetParams<Req>): Promise<HttpResponse<Res>> {
+    const httpResponse = await axios.get(params.url);
+
     return {
       statusCode: httpResponse.status,
-      // cast here is safe because caller will type the client appropriately
-      body: httpResponse.data as Res
-    }
+      body: httpResponse.data as Res,
+    };
+  }
+
+  async post(params: HttpPostParams<Req>): Promise<HttpResponse<Res>> {
+    const httpResponse = await axios.post(params.url, params.body);
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data as Res,
+    };
   }
 }
