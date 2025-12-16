cd0f6f451a8fc39f0777c02137d154581fd902d5
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-12-16 07:33:26 -0300

    feat: ajusta o retorno do método mapView para usar ApplicationScheduleByRiskDto diretamente

--- src/data/usecases/remote-get-application-schedule-map-by-risk.ts
+++ src/data/usecases/remote-get-application-schedule-map-by-risk.ts
@@ -1,16 +1,14 @@
 import { GetApplicationScheduleMapByRisk, Location } from '@/domain/usecases/get-application-schedule-map-by-risk'
-import { mapApplicationScheduleDtoToModel } from '@/lib/mappers/applicationScheduleMapper'
-import { ApplicationScheduleByRiskModel } from '@/domain/models/application-schedule-by-risk-model'
+import { ApplicationScheduleByRiskDto } from '@/lib/types/ApplicationScheduleByRiskDto'
 import { HttpGetClient } from '@/data/protocols/http/http-get-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { BadRequestError } from '@/domain/errors/bad-request-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { ApplicationScheduleByRiskDto } from '@/lib/types/ApplicationScheduleByRiskDto'
 export class RemoteGetApplicationScheduleMapByRisk implements GetApplicationScheduleMapByRisk {
   constructor(private readonly url: string, private readonly httpGetClient: HttpGetClient<Location | void, ApplicationScheduleByRiskDto>) {}
 
-  async mapView(location?: Location): Promise<ApplicationScheduleByRiskModel> {
+  async mapView(location?: Location): Promise<ApplicationScheduleByRiskDto> {
     const url = this.url
 
   const body = location ?? undefined
@@ -18,7 +16,7 @@ export class RemoteGetApplicationScheduleMapByRisk implements GetApplicationSche
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        return mapApplicationScheduleDtoToModel(httpResponse.body!)
+        return httpResponse.body!
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
       case HttpStatusCode.badRequest:
