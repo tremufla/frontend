b95ceba64d4d96a017c3760da8c686be4e1bc2e4
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-12-17 09:59:44 -0300

    feat: adiciona suporte para dados de agendamento no componente MapClient

--- src/components/map/MapClient.tsx
+++ src/components/map/MapClient.tsx
@@ -1,12 +1,19 @@
-"use client"
+'use client';
 
-import 'leaflet/dist/leaflet.css'
-import React from 'react'
-import dynamic from 'next/dynamic'
-import { PropertyModel } from '@/domain/models/property-model'
+import 'leaflet/dist/leaflet.css';
+import React from 'react';
+import dynamic from 'next/dynamic';
+import { PropertyModel } from '@/domain/models/property-model';
+import { ApplicationScheduleByRiskModel } from '@/domain/models/application-schedule-by-risk-model';
 
-const Map = dynamic(() => import('@/components/ui/map/Map'), { ssr: false })
+const Map = dynamic(() => import('@/components/ui/map/Map'), { ssr: false });
 
-export default function MapClient({ properties }: { properties?: PropertyModel[] }) {
-  return <Map properties={properties} />
+export default function MapClient({
+  properties,
+  scheduleData,
+}: {
+  properties?: PropertyModel[];
+  scheduleData?: ApplicationScheduleByRiskModel;
+}) {
+  return <Map properties={properties} scheduleData={scheduleData} />;
 }
