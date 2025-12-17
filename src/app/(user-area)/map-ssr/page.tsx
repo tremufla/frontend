7b9fcad438d016305e052c43151f7be0506142e9
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-12-17 09:56:42 -0300

    feat: ajusta a lógica de busca de dados de agendamento por risco e integra ao componente de mapa

--- src/app/(user-area)/map-ssr/page.tsx
+++ src/app/(user-area)/map-ssr/page.tsx
@@ -1,15 +1,19 @@
 import Header from '@/components/ui/header/hearder'
-import HeaderClient from '@/components/map/HeaderClient'
+import HeaderClient, { Location } from '@/components/map/HeaderClient'
 import MapClient from '@/components/map/MapClient'
 import { makeRemoteGetAllProperties } from '@/main/factories/usecases/remote-get-all-properties-factory'
 import { PropertyModel } from '@/domain/models/property-model'
+import { makeRemoteGetApplicationScheduleMapByRisk } from '@/main/factories/usecases/remote-get-application-schedule-map-by-risk-factory'
+import { ApplicationScheduleByRiskModel } from '@/domain/models/application-schedule-by-risk-model'
 
 export const revalidate = 0
 
 async function fetchProperties(): Promise<PropertyModel[]> {
   try {
-    const getAll = makeRemoteGetAllProperties()
-    const data = await getAll.getAll()
+    const useCase = makeRemoteGetAllProperties()
+
+    const data = await useCase.getAll()
+
     return data ?? []
   } catch (err) {
     console.error('Error fetching properties on server:', err)
@@ -17,14 +21,33 @@ async function fetchProperties(): Promise<PropertyModel[]> {
   }
 }
 
+async function fetchScheduleData(locations: Location[]): Promise<ApplicationScheduleByRiskModel> {
+  try {
+    if (locations.length === 0) return { danger: [], warning: [], safe: [] }
+
+    const firstLocation = locations[0].coordinates
+
+    const useCase = makeRemoteGetApplicationScheduleMapByRisk()
+
+    const data = await useCase.mapView(firstLocation)
+
+    return data ?? { danger: [], warning: [], safe: [] }
+  } catch (err) {
+    console.error('Error fetching properties on server:', err)
+    return { danger: [], warning: [], safe: [] }
+  }
+}
+
 export default async function MapSSRPage() {
   const properties = await fetchProperties()
 
-  const locations = (properties ?? []).map((p) => ({
-    id: p.id,
-    name: p.name,
-    coordinates: { latitude: p.latitude, longitude: p.longitude },
+  const locations = (properties ?? []).map((property) => ({
+    id: property.id,
+    name: property.name,
+    coordinates: { latitude: property.latitude, longitude: property.longitude },
   }))
+
+  const scheduleData = await fetchScheduleData(locations)
 
   return (
     <main className="p-4">
@@ -33,7 +56,7 @@ export default async function MapSSRPage() {
       </Header>
 
       <div className="mt-4">
-        <MapClient properties={properties} />
+        <MapClient properties={properties} scheduleData={scheduleData} />
       </div>
     </main>
   )
