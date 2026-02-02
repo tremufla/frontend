f4ed93e0c6dad5c15e5760cc15a027a2b7475258
Cesar Pires <cesar.augusto.pires@hotmail.com>
2026-02-01 21:10:17 -0300

    refactor: melhora a estrutura do método mapView no RemoteGetApplicationScheduleMapByRisk

--- src/data/usecases/remote-get-application-schedule-map-by-risk.ts
+++ src/data/usecases/remote-get-application-schedule-map-by-risk.ts
@@ -5,24 +5,36 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { BadRequestError } from '@/domain/errors/bad-request-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+
 export class RemoteGetApplicationScheduleMapByRisk implements GetApplicationScheduleMapByRisk {
+
   constructor(private readonly url: string, private readonly httpGetClient: HttpGetClient<Location | void, ApplicationScheduleByRiskDto>) {}
 
   async mapView(location?: Location): Promise<ApplicationScheduleByRiskDto> {
-    const url = this.url
+    const isDev = process.env.IS_DEV === "true";
 
-  const body = location ?? undefined
-  const httpResponse = await this.httpGetClient.get({ url, body })
+    if (isDev) {
+      return {
+        danger: [],
+        warning: [],
+        safe: [],
+      };
+    }
+
+    const url = this.url;
+
+    const body = location ?? undefined;
+    const httpResponse = await this.httpGetClient.get({ url, body });
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        return httpResponse.body!
+        return httpResponse.body!;
       case HttpStatusCode.unauthorized:
-        throw new InvalidCredentialsError()
+        throw new InvalidCredentialsError();
       case HttpStatusCode.badRequest:
-        throw new BadRequestError()
+        throw new BadRequestError();
       default:
-        throw new UnexpectedError()
+        throw new UnexpectedError();
     }
   }
 }
